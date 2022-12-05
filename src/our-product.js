9ca3f0374ff4f353bd55d731bf0a08ce74d76ff3
Nathan Klassen <nathanklassen7@gmail.com>
2022-12-05 13:20:19 -0500

    Added products section

--- src/our-product.js
+++ src/our-product.js
@@ -1,19 +1,54 @@
 import Fade from 'react-reveal/Fade';
-
+import FullModel from './assets/our-product/full-model.png';
+import Press from './assets/our-product/press.png';
+import ModelNoPress from './assets/our-product/model-no-press.png';
+import Spacer from './spacer';
+import { sharedStyles } from './shared-styles';
 const OurProduct = ({ show }) => {
   const style = {
     container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
       opacity: show ? 1 : 0,
     },
-    header: {
-      fontSize: 40,
-      fontWeight: 'bold',
+    row: {
+      display: 'flex',
     },
+    image: { height: '300px' },
   };
   return (
     <Fade when={show}>
       <div style={style.container}>
-        <div style={style.header}>Our Product</div>
+        <div style={sharedStyles.header}>Our Product</div>
+        <div style={sharedStyles.detailsContainer}>
+          <div style={sharedStyles.details}>
+            With a sleek design that fits naturally in any modern kitchen and
+            detachable components that make it a breeze to clean and maintain,
+            this machine will take your morning routine to the next level of
+            comfort and convenience.
+          </div>
+        </div>
+        <Spacer height={'30px'} />
+        <div style={style.row}>
+          <img
+            style={style.image}
+            src={FullModel}
+            alt='full model of coffee maker'
+          />
+          <Spacer width={'60px'} />
+          <img
+            style={style.image}
+            src={ModelNoPress}
+            alt='model of coffee maker without press'
+          />
+          <Spacer width={'60px'} />
+          <img
+            style={style.image}
+            src={Press}
+            alt='model of coffee maker press'
+          />
+        </div>
       </div>
     </Fade>
   );
---
+++ src/shared-styles.js
@@ -0,0 +1,18 @@
+export const sharedStyles = {
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  detailsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    maxWidth: 650,
+  },
+  details: {
+    fontSize: 20,
+    color: 'black',
+    textShadow: 'none',
+    textAlign: 'center',
+  },
+};
